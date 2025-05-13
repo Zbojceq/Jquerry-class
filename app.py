44345e813442cbeb19f959402e629b119c7ad83b
Kinga Iwaniuk <kiwaniuk@student.agh.edu.pl>
2025-05-13 12:48:49 +0200

    re

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+    
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/method', methods=['GET', 'POST'])
+def method():
+    if request.method == 'POST':
+        return 'Użyto metody POST'
+    else:
+        return 'Użyto metody GET. '
+    
+@app.route('/hello/<name>')
+def hello_name(name):
+    return f'Witaj, {name}!'
